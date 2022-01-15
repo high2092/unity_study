bb79c55dd0f9fcfe8c19a2089a335e7c8bc01eee
hhyuw <high2092@gmail.com>
2022-01-16 02:48:47 +0900

    CatEscape: 충돌 판정 추가

--- CatEscape/Assets/ArrowController.cs
+++ CatEscape/Assets/ArrowController.cs
@@ -4,19 +4,31 @@ using UnityEngine;
 
 public class ArrowController : MonoBehaviour
 {
+    GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.player = GameObject.Find("player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, -0.1f, 0);
+        transform.Translate(0, -0.01f, 0);
 
         if (transform.position.y < -5.0f) {
-            Destroy(gameObject); // 메모리 해제; gameObject: 객체 자신을 나타냄 (this 같은)
+            Destroy(gameObject); // 메모리 해제; gameObject: 객체 자신을 나타냄; Q. this랑 무슨 차이?
+        }
+
+        Vector2 p1 = transform.position;
+        Vector2 p2 = this.player.transform.position;
+        Vector2 dir = p1 - p2;
+        float d = dir.magnitude;
+
+        float r1 = 0.5f, r2 = 0.85f; // 각각 화살과 플레이어의 반지름
+
+        if (d <= r1+r2) {
+            Destroy(gameObject);
         }
     }
 }
