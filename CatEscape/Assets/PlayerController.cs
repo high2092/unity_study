337a4c2aa19d77a59f4408e42a58827c96b3833c
hhyuw <high2092@gmail.com>
2022-01-16 03:24:18 +0900

    CatEscape: 버튼 조작으로 변경

--- CatEscape/Assets/PlayerController.cs
+++ CatEscape/Assets/PlayerController.cs
@@ -13,12 +13,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-            transform.Translate(-3, 0, 0);
-        }
+        
+    }
+    
+    public void LButtonDown()
+    {
+        transform.Translate(-3, 0, 0);
+    }
 
-        if (Input.GetKeyDown(KeyCode.RightArrow)) {
-            transform.Translate(3, 0, 0);
-        }
+    public void RButtonDown()
+    {
+        transform.Translate(3, 0, 0);
     }
 }
