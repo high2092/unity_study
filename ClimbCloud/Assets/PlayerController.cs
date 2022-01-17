68ae0dd9cf0c764fcc789a7511fb0722a367c63d
hhyuw <high2092@gmail.com>
2022-01-17 17:03:00 +0900

    ClimbCloud: 점프 애니메이션 추가; 스마트폰 조작으로 변경;

--- ClimbCloud/Assets/PlayerController.cs
+++ ClimbCloud/Assets/PlayerController.cs
@@ -8,8 +8,10 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rigid2D;
     Animator animator;
     float jumpForce = 680.0f;
-    float walkForce = 3.0f;
+    float walkForce = 30.0f;
     float maxWalkSpeed = 2.0f;
+    float threshold = 0.2f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +22,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && this.rigid2D.velocity.y == 0) {
+        if (Input.GetMouseButtonDown(0) && this.rigid2D.velocity.y == 0) {
+            this.animator.SetTrigger("JumpTrigger");
             this.rigid2D.AddForce(transform.up * this.jumpForce);
         }
         if (transform.position.y < -10) {
@@ -31,8 +34,8 @@ public class PlayerController : MonoBehaviour
 
         int key = 0;
 
-        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
-        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+        if (Input.acceleration.x > this.threshold) key = 1;
+        if (Input.acceleration.x < -this.threshold) key = -1;
 
         float speedx = Mathf.Abs(this.rigid2D.velocity.x);
 
@@ -43,6 +46,11 @@ public class PlayerController : MonoBehaviour
         if (key != 0) transform.localScale = new Vector3(key, 1, 1);
 
         this.animator.speed = speedx / 2.0f;
+        if (this.rigid2D.velocity.y == 0) {
+            this.animator.speed = speedx / 2.0f;
+        } else {
+            this.animator.speed = 1.0f;
+        }
 
         
     }
