36f6e10b176510cd275abf7cb2780b6be3a6ca8c
hhyuw <high2092@gmail.com>
2022-06-29 02:57:08 +0900

    DeliveryDriver: GetComponent를 통한 객체 색 변경

--- DeliveryDriver/Assets/Trigger.cs
+++ DeliveryDriver/Assets/Trigger.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class Trigger : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+
+    SpriteRenderer spriteRenderer;
+
+    private void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Package") {
             Debug.Log("Package picked up");
+            spriteRenderer.color = hasPackageColor;
             Destroy(other.gameObject, 0);
         } else {
-            Debug.Log("triggered");
+            Debug.Log("Package delivered");
+            spriteRenderer.color = noPackageColor;
         }
     }
 }
