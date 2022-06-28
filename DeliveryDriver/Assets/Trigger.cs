9ff40df564cd63da8b06338a700e2f1c65fadd6c
hhyuw <high2092@gmail.com>
2022-06-29 02:44:52 +0900

    DeliveryDriver: Destroy

--- DeliveryDriver/Assets/Trigger.cs
+++ DeliveryDriver/Assets/Trigger.cs
@@ -7,6 +7,7 @@ public class Trigger : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Package") {
             Debug.Log("Package picked up");
+            Destroy(other.gameObject, 0);
         } else {
             Debug.Log("triggered");
         }
