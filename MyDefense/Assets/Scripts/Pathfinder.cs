ea91cf00ce4934b6044f975932f85b01d0c58702
hhyuw <high2092@gmail.com>
2022-06-29 17:56:08 +0900

    MyDefense: Spawner Script 작성

--- MyDefense/Assets/Scripts/Pathfinder.cs
+++ MyDefense/Assets/Scripts/Pathfinder.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class Pathfinder : MonoBehaviour
 {
-    [SerializeField] WaveConfigSO waveConfig;
+    EnemySpawner enemySpawner;
+    WaveConfigSO waveConfig;
     List<Transform> waypoints;
     int waypointIndex = 0;
+
+    void Awake() {
+        enemySpawner = FindObjectOfType<EnemySpawner>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        waveConfig = enemySpawner.GetCurrentWave();
         waypoints = waveConfig.GetWaypoints();
         transform.position = waypoints[waypointIndex].position;
     }
--- MyDefense/Assets/Scripts/WaveConfigSO.cs
+++ MyDefense/Assets/Scripts/WaveConfigSO.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Wave Config", fileName = "New Wave Config")]
 public class WaveConfigSO : ScriptableObject
 {
+    [SerializeField] List<GameObject> enemyPrefabs;
     [SerializeField] Transform pathPrefab;
     [SerializeField] float moveSpeed = 5f;
 
-    // public Transform GetStartingWaypoint() {
-    //     return pathPrefab.GetChild(0);
-    // }
+    public Transform GetStartingWaypoint() {
+        return pathPrefab.GetChild(0);
+    }
+    
+    public int GetEnemyCount() {
+        return enemyPrefabs.Count;
+    }
+
+    public GameObject GetEnemyPrefab(int idx) {
+        return enemyPrefabs[idx];
+    }
 
     public List<Transform> GetWaypoints() {
         List<Transform> waypoints = new List<Transform>();
